702cb599dee8ebc6f90269cbd4c02b81eccb1ce9
maggask <maggask@gmail.com>
2014-11-03 23:39:20 +0000

    Client side code working

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,9 @@
 var express = require('express'),
-    routes = express.Router();
+    routes = express.Router(),
+    path = require('path'),
+    http = require('http'),
+    request = require('request'),
+    requestify = require('requestify');
 
 
 app = express();
@@ -12,14 +16,46 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/', routes);
 
-app.get('/', function(req, res) {
-    res.render('index');
+app.get('/', function(req, res) { 
+    res.render('index')
 });
 
 app.get('/kodemon/index', function(req, res) {
+    
+    requestify.get("http://localhost:4000/api/entries/keys").then(function(response) {
 
+        var response = response.getBody();
+
+        res.render('keys', {data: response});
+    });
 });
 
-app.get('/kodemon/function/:function', function(req, res) {
+app.get('/kodemon/index/function', function(req, res) {
+    console.log(req);
+    var key = req.query.key1;
+    requestify.get("http://localhost:4000/api/entries/key/" + key).then(function(response) {
 
+        var response = response.getBody();
+        console.log(response);
+
+        res.render('key', {data: response});
+    });
+});
+
+app.get('/kodemon/index/function/range', function(req, res) {
+    console.log(req);
+    var key = req.query.key2;
+    var from = "/" + req.query.timeFrom;
+    var to = "/" + req.query.timeTo;
+    requestify.get("http://localhost:4000/api/entries/" + key + from + to).then(function(response) {
+
+        var response = response.getBody();
+        console.log(response);
+
+        res.render('keyfromto', {data: response});
+    });
+});
+
+app.listen(5000, function() {
+    console.log('Server is ready');
 });
--- frontend/public/js/graph.js
+++ frontend/public/js/graph.js
@@ -8,21 +8,7 @@ $(function () {
 
         $('#container').highcharts({
             chart: {
-                type: 'spline',
-                animation: Highcharts.svg, // don't animate in old IE
-                marginRight: 10,
-                events: {
-                    load: function () {
-
-                        // set up the updating of the chart each second
-                        var series = this.series[0];
-                        setInterval(function () {
-                            var x = (new Date()).getTime(), // current time
-                                y = Math.random();
-                            series.addPoint([x, y], true, true);
-                        }, 1000);
-                    }
-                }
+                type: 'spline'
             },
             title: {
                 text: 'Live random data'
